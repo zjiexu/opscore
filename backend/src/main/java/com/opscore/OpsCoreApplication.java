e736a94e0e01908d26f76a70e506a4056d298ecc
zjiexu <zjiexuo@gmail.com>
2026-08-11 18:46:25 -0500

    chore: add Spring Boot backend scaffold

---
+++ backend/src/main/java/com/opscore/OpsCoreApplication.java
@@ -0,0 +1,12 @@
+package com.opscore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OpsCoreApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(OpsCoreApplication.class, args);
+  }
+}
